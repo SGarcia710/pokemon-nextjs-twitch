f9ccc7c2adf5ed908252d4698ddb355710d5ead0
Sebastian Garcia <sgarcia710@outlook.com>
2021-03-15 22:02:50 -0500

    Day 1

---
+++ pages/about-me.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutMePage = () => {
+  return (
+    <div>
+      <h1>Esto es algo de información sobre mí</h1>
+    </div>
+  );
+};
+
+export default AboutMePage;
---
+++ pages/contact/enterprise.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EnterprisePage = () => {
+  return (
+    <div>
+      <h1>Este es el contacto empresarial</h1>
+    </div>
+  );
+};
+
+export default EnterprisePage;
